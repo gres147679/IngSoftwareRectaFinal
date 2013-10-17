0056cf6d6a1b7a4ad5170599e587d4327ab1d57f
rosangelisgarcia <rosangelis.92@gmail.com>
2013-10-17 17:38:08 -0430

    Login de usuarios

--- Tarea5/Mocel/Mocel/WebAccess/urls.py
+++ Tarea5/Mocel/Mocel/WebAccess/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('Mocel.WebAccess.views',
 	url(r'^cliente/$','ver_clientes'),
 	url(r'^cliente/(\d+)/$','ver_productos'),
 	url(r'^producto/(\d+)/$','info_producto'),
+	url(r'^login/$','login_view',name='vista_login'),
+	url(r'^logout/$','logout_view',name='vista_logout'),
 )
--- Tarea5/Mocel/Mocel/WebAccess/views.py
+++ Tarea5/Mocel/Mocel/WebAccess/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-from Mocel.WebAccess.forms import AgregarClienteForm
+from Mocel.WebAccess.forms import AgregarClienteForm, loginForm
 from Mocel.WebAccess.models import Cliente, Producto, Activa, Afilia
 from Mocel.views import generarFactura
+from django.contrib.auth import login,logout,authenticate
+from django.http import HttpResponseRedirect
 
 def index_view(request):
 	return render_to_response('index.html',context_instance = RequestContext(request))
@@ -85,3 +87,31 @@ def info_producto(request, serieprod):
     return render(request, 'infoPostpago.html', context)
   
   return render(request, 'infoSinPlan.html', context)
+
+
+def login_view(request):
+  	mensaje = ""
+  	if request.user.is_authenticated():
+  		return HttpResponseRedirect('/')
+  	else:
+  		if request.method == "POST":
+			form = loginForm(request.POST)
+			info = "Inicializando"
+
+			if form.is_valid():
+				username = form.cleaned_data['username']
+				password = form.cleaned_data['password']
+				usuario = authenticate(username=username,password=password)
+
+				if usuario is not None and usuario.is_active:
+					login(request,usuario)
+					return HttpResponseRedirect('/')
+				else:
+					mensaje = "ERROR: usuario y/o password incorrecto."
+		form = loginForm()
+		ctx = {'form':form, 'mensaje':mensaje}
+		return render_to_response('login.html',ctx,context_instance=RequestContext(request))
+		
+def logout_view(request):
+	logout(request)
+	return HttpResponseRedirect('/')
