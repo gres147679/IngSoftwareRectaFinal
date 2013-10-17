3abae94e4758b4e225b989f651c66513e7f5a3d0
Gamar <gamarazuaje@gmail.com>
2013-10-17 15:39:09 -0430

    Admin arreglado

--- Tarea5/Mocel/Mocel/WebAccess/urls.py
+++ Tarea5/Mocel/Mocel/WebAccess/urls.py
@@ -5,6 +5,5 @@ urlpatterns = patterns('Mocel.WebAccess.views',
 	url(r'^agregarCliente/$','agregar_cliente_view',name="vista_agregar_cliente"),
 	url(r'^cliente/$','ver_clientes'),
 	url(r'^cliente/(\d+)/$','ver_productos'),
-	url(r'^cliente/\d+/producto/\d+/$','index'),
 	url(r'^producto/(\d+)/$','info_producto'),
 )
