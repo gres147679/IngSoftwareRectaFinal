7d22634f42f77db27dae2f8911d6e04bca48519a
Gamar <gamarazuaje@gmail.com>
2013-10-18 05:09:45 -0430

    Actualizado pruebas

--- Tarea5/Mocel/Mocel/WebAccess/tests.py
+++ Tarea5/Mocel/Mocel/WebAccess/tests.py
@@ -9,12 +9,6 @@ from django.test import TestCase
 from models import *
 from views import generarFactura
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
                 
 class PruebaPlan(TestCase):
     def setUp(self):
